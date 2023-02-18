fd8c4b41c977d110714067ef53661339694f6bae
BarokDG <it.barok.dagim@gmail.com>
2023-02-18 14:55:41 +0300

    Add notification components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import RequestPickup from "./components/RequestPickup";
 import LookForShipment from "./components/LookForShipment";
+import ShipmentRequestNotification from "./components/notifications/ShipmentRequestNotification";
 
 import "./App.css";
 
@@ -8,6 +9,12 @@ function App() {
     <div className="App">
       <RequestPickup />
       <LookForShipment />
+      <ShipmentRequestNotification
+        requestFrom="Requester"
+        pickup="Detroit"
+        destination="Chicago"
+        shipmentDescription="Description of load and special features requested"
+      />
     </div>
   );
 }
--- src/components/RequestPickup.tsx
+++ src/components/RequestPickup.tsx
@@ -1,45 +1,57 @@
 import React from "react";
+import { useState } from "react";
 
 export default function RequestPickup() {
+  const [isRequestSubmitted, setIsRequestSubmitted] = useState<boolean>(false);
+
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    setIsRequestSubmitted(true);
     alert("Will implement this");
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Request a pickup</h1>
-      <ul>
-        <li>
-          <label htmlFor="date">When?</label>
-          <input type="date" name="shipping_date" id="date" />
-        </li>
-        <li>
-          <label htmlFor="pickup">
-            Starting city(zip or city, state, country)
-          </label>
-          <input type="text" name="starting_city" id="pickup" />
-        </li>
-        <li>
-          <label htmlFor="destination">
-            Ending city(zip or city, state, country)
-          </label>
-          <input type="text" name="destination_city" id="destination" />
-        </li>
-        <li>
-          <label htmlFor="weight">Weight of shipment</label>
-          <input type="number" name="shipment_weight" id="weight" />
-        </li>
-        <li>
-          <label htmlFor="details">Short description of shipment</label>
-          <textarea name="shipment_details" id="details"></textarea>
-        </li>
-        <li>
-          <label htmlFor="safe">Required for safe shipment?</label>
-          <input type="checkbox" name="safe_shipment" id="safe" />
-        </li>
-        <button type="submit">Submit</button>
-      </ul>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Request a pickup</h1>
+        <ul>
+          <li>
+            <label htmlFor="date">When?</label>
+            <input type="date" name="shipping_date" id="date" />
+          </li>
+          <li>
+            <label htmlFor="pickup">
+              Starting city(zip or city, state, country)
+            </label>
+            <input type="text" name="starting_city" id="pickup" />
+          </li>
+          <li>
+            <label htmlFor="destination">
+              Ending city(zip or city, state, country)
+            </label>
+            <input type="text" name="destination_city" id="destination" />
+          </li>
+          <li>
+            <label htmlFor="weight">Weight of shipment</label>
+            <input type="number" name="shipment_weight" id="weight" />
+          </li>
+          <li>
+            <label htmlFor="details">Short description of shipment</label>
+            <textarea name="shipment_details" id="details"></textarea>
+          </li>
+          <li>
+            <label htmlFor="safe">Required for safe shipment?</label>
+            <input type="checkbox" name="safe_shipment" id="safe" />
+          </li>
+          <button type="submit">Submit</button>
+        </ul>
+      </form>
+      {isRequestSubmitted && (
+        <p style={{ background: "darkgreen", color: "white" }}>
+          Your request has been sent out. We will notify you when a truck has
+          accepted your request.
+        </p>
+      )}
+    </>
   );
 }
